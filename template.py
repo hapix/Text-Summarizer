1a6472d4e02d6191d646f0b5cb4f14865057b712
hapix <itsc23@yahoo.com>
2023-12-30 00:26:31 +0100

    Re start the project

--- template.py
+++ template.py
@@ -11,7 +11,7 @@ ist_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/_ _init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
